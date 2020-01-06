6b6cc779d3070b88d60e869ece1305684cf506cf
Sander Moen <smoensander@gmail.com>
2020-01-07 00:30:24 +0100

    created a Player class as well as a PlayerConfig and PlayerData class to store information about the player and the players settings

--- sockets/classes/Orb.js
+++ sockets/classes/Orb.js
@@ -1,8 +1,8 @@
 class Orb {
-  constructor() {
+  constructor(settings) {
     this.color = this.getRandomColor();
-    this.locX = Math.floor(Math.random() * 500);
-    this.locY = Math.floor(Math.random() * 500);
+    this.locX = Math.floor(Math.random() * settings.worldWidth);
+    this.locY = Math.floor(Math.random() * settings.worldHeight);
     this.radius = 5;
   }
   getRandomColor() {
--- sockets/socketMain.js
+++ sockets/socketMain.js
@@ -1,12 +1,32 @@
 // Where all our main socket stuff will go
 const io = require('../servers').io;
 
+const Player = require('./classes/Player');
+const PlayerConfig = require('./classes/PlayerConfig');
+const PlayerData = require('./classes/PlayerData');
 const Orb = require('./classes/Orb');
+
 let orbs = [];
+let settings = {
+  defaultOrbs: 500,
+  defaultSpeed: 6,
+  defaultSize: 6,
+  // as a player gets bigger the zoom needs to go out
+  defaultZoom: 1.5,
+  worldWidth: 500,
+  worldHeight: 500
+};
 
 initGame();
 
 io.sockets.on('connect', socket => {
+  // a player has connected
+  // make a playerConfig object
+  let playerConfig = new PlayerConfig(settings);
+  // make a player data object
+  let playerData = new PlayerData(null, settings);
+  // make a master player object to hold both
+  let player = new Player(socket.id, playerConfig, playerData);
   socket.emit('init', {
     orbs
   });
@@ -14,8 +34,8 @@ io.sockets.on('connect', socket => {
 
 // Run at the beginning of a new game
 function initGame() {
-  for (let i = 0; i < 500; i++) {
-    orbs.push(new Orb());
+  for (let i = 0; i < settings.defaultOrbs; i++) {
+    orbs.push(new Orb(settings));
   }
 }
 
